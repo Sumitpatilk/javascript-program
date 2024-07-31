5caaccaea8ee8c83bc41c145e3ed9093199cd84d
Sumitpatilk <sumitspatil212@gmail.com>
2024-07-31 17:49:55 +0530

    declare and initialization variable assignment-1
     declare and initialization variable assignment-1

---
+++ Assignment-1.js
@@ -0,0 +1,51 @@
+
+var myFullName; 
+
+console.log(myFullName);
+
+myFullName = "Sumit Patil";
+
+console.log(myFullName);
+
+myFullName = "Sumit Sudhakar Patil";
+
+console.log(myFullName);
+
+
+// 2. Declare and initialize a variable with name 'myLove' with value → "I love only JS" and log result on console
+
+var myLove = "I love only JS";
+
+console.log(myLove);
+
+
+// 3. Create a variable for form controls as listed below. Declare and initialize must be on same line
+
+var firstName = "Sumit";
+
+var lastName = "Patil";
+
+var age = 25;
+
+var address = "Shikashak colony raver";
+
+var pinCode = "425507";
+
+var gender = "Male";
+
+var isMarried = true;
+
+
+console.log(firstName);
+
+console.log(lastName);
+
+console.log(age);
+
+console.log(address);
+
+console.log(pinCode);
+
+console.log(gender);
+
+console.log(isMarried);

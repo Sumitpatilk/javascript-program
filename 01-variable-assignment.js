962dd515bcef4e280c5f77d5719a2e57cbb6a40e
Sumitpatilk <sumitspatil212@gmail.com>
2024-07-31 17:42:03 +0530

    variable personal detail page
    variable personal detail page

---
+++ 01-variable-assignment.js
@@ -0,0 +1,6 @@
+
+console.log("My city name is : Raver.")
+console.log("My best friend's name is : vipul.")
+console.log("My country name is : India.")
+console.log("My school name is : Sane Guruji Nutan Madhyamic Vidyalaya.")
+console.log("My college name is : SSBT COET, Jalgaon.")

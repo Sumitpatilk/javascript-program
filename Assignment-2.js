dd9f2ccacc5331a8412f608eecfe08408e6fa12a
Sumitpatilk <sumitspatil212@gmail.com>
2024-07-31 17:52:14 +0530

    create a variable  file typeOf,  variable, datatype and types of operator page
    create a variable  file typeOf,  variable, datatype and types of operator page

---
+++ Assignment-2.js
@@ -0,0 +1,50 @@
+console.log("===============================1=======================");
+var bankName; 
+console.log("Bank NAme Is :",bankName);
+
+bankName = "SBI Bank Name";
+console.log("Bank Name Is :",bankName);
+var Bankname = typeof bankName;
+console.log("Bank Name Datatype Is:", Bankname);
+
+// 2. Declare and initialize a variable with name 'myLove' with value → "I love only JS" and log result on console
+console.log("===============================2=======================");
+var marks = "93%";
+console.log("My Mark Are: ", marks);
+var marksTypeOf = typeof marks;
+console.log("My Marks Datatype Is :", marksTypeOf);
+console.log("===============================3=======================");
+// 3. Create a variable for form controls as listed below. Declare and initialize must be on same line
+
+var workstatus;
+console.log("My working Status Is :",workstatus);
+
+
+// var workingstatus = "true";
+// console.log("My working Status Is:",workingstatus);
+
+var isMarried = true;
+console.log("My working Status Is : ", isMarried);
+var isMarriedTypeOf = typeof isMarried;
+console.log("Is Working Datatype : ", isMarriedTypeOf);
+
+console.log("===============================4=======================");
+console.log("---------------------------a-------------------------");
+var Value = 'Hundred And Seven';
+console.log("Total Count Is:", Value);
+var ValueTypeOf = typeof Value;
+console.log("Total Count Datatype Is :", ValueTypeOf);
+console.log("---------------------------b-------------------------");
+var value = 107;
+console.log("Total Count Is:", value);
+var valueTypeOf = typeof value;
+console.log("Total Count Datatype After Updating: ", valueTypeOf);
+
+
+
+
+
+
+
+
+

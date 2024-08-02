68a4c2350ed7721941906a09b0626f337cd05af9
Sumitpatilk <sumitspatil212@gmail.com>
2024-08-02 18:34:10 +0530

    create function assignment 4
    create function assignment 4

---
+++ Assignment-4.js
@@ -0,0 +1,13 @@
+function bankDetails(bankName, accountNum,  location, pinCode){
+    // console.log("My bank Name Is :",bankName);
+    // console.log("My bank Name Is : ",accountNum);
+    // console.log("My bank Name Is :",location);
+    // console.log("My bank Name Is",pinCode);
+    console.log("My bank Name Is :",bankName, accountNum, location,pinCode);
+    
+    
+
+}
+bankDetails("CITI Bank", "3456782345","Pune","431202");
+bankDetails("Axis Bank","7856782345","Mumbai","441202");
+bankDetails("HDFC Bank","8956782345","Pune","631202","Open")

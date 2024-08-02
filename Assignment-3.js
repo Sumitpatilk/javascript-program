fd28f25216035c81c6b54c1acfc2b4099b30488d
Sumitpatilk <sumitspatil212@gmail.com>
2024-08-02 18:33:23 +0530

    create function assignment 3
    create function assignment 3

---
+++ Assignment-3.js
@@ -0,0 +1,27 @@
+console.log("=============function with no argument and no return value=============");
+
+
+function show(){
+    console.log(" Mistake are proof that you are trying, learning and growing you are enough, just as you are");
+}
+show();
+
+console.log("======== function with arguments and no return value =============");
+function personaldetail(Firstname, Lastname, Collegename ){
+    console.log("My First Name Is:",Firstname);
+    console.log("My Last Name Is : ",Lastname);
+    console.log("My College Name Is :",Collegename);
+    
+}
+personaldetail("Sumit","Patil","SSBTs Colg. Of Eng. and Tec.Jalgaon");
+
+console.log("======== function with arguments and no return value =============");
+function addThreeVlaues(parm1,parm2,parm3){
+    var result= parm1 +parm2+ parm3;
+    console.log("addition Is:",result);
+    
+}
+addThreeVlaues(10.23,600,40);
+addThreeVlaues("hello"," good"," morning")
+
+

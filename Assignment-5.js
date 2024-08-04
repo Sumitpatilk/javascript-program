d998d8472b47f10eef5d60df7a7acbba0aa6a5d6
Sumitpatilk <sumitspatil212@gmail.com>
2024-08-04 18:12:36 +0530

    create a basic string assignment using String Template
    create a basic string assignment using String Template

---
+++ Assignment-5.js
@@ -0,0 +1,11 @@
+
+var stringBasics=" Google or Microsoft";
+console.log("My Given Company Is :",stringBasics);
+
+var Hobbies1 = "Reading History";
+var Hobbies2 = " Watching SCi-Fi, mythological ,history series and action movies";
+var Hobbies3 = " Programmimg";
+var concatString = Hobbies1 + Hobbies2+ Hobbies3;
+var result = Hobbies1.concat(Hobbies2+Hobbies3);
+console.log("My Hobbies are:",result );
+console.log("Total number of charcter in hobbies",result.length);

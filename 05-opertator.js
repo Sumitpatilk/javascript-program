00698a6fb3fa53cb682849468743719e37f2fefc
Sumitpatilk <sumitspatil212@gmail.com>
2024-08-06 14:08:42 +0530

    create demo operator
    create demo operator

---
+++ 05-opertator.js
@@ -0,0 +1,12 @@
+
+   var n1 = 10;
+var n2 = 5;
+
+n2+=20;// n2 = n2+20
+console.log(`Compound Addition is: ${n2}`);
+
+var n1 = "50";
+var n2 = 50;
+//var result = n1 == n2;
+console.log(n1 == n2);
+console.log(n1 === n2);
